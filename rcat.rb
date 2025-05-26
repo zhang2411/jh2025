3023e8e4bfdefe477e9573c6d1e0b09527164c07
zhang2411 <zhabgdinglong@gmail.com>
2025-05-26 11:01:20 +0900

    Update rcat.rb

--- rcat.rb
+++ rcat.rb
@@ -14,10 +14,28 @@ end
 #file = File.open("reader.rb", "r")
 #while a
 #使用command line将文件内容一行行表示
-#ruby racat.rb a.txt b.txt c.rb
-File.open ARGV[0], "r" do |file|
-    
+
+ARGV.each do |arg|
+    File.open arg, "r" do |file|
+
     while line = file.gets&.chomp
         puts line
     end
 end
+
+end
+
+#ruby racat.rb a.txt b.txt c.rb表述显示的文件是那个
+File.open ARGV[0], "r" do |file|
+
+    while line = file.gets&.chomp
+        puts line
+    end
+end
+
+#drwxrwxrxw
+# User Group Other
+#r	read = 可读
+#w	write = 可写
+#x	execute = 可执行（或目录可进入）
+#-	没有该权限

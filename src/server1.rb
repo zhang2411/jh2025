86d839635223923e0dadb8bc5479f05db0605b5b
zhang2411 <zhabgdinglong@gmail.com>
2025-06-16 12:25:18 +0900

    Thread

--- src/server1.rb
+++ src/server1.rb
@@ -5,11 +5,17 @@ require "socket"
 def server sock
   while buf = sock.gets
       p buf  # Print the received data to the console
+      sock.puts "reply:#{buf}" # Echo the received data back to the client
   end
   sock.close  # Close the socket when done
 end
 
 s0 = TCPServer.open(80)  # Listen on port 2000
-sock = s0.accept  # Wait for a client to connect
-server sock
+while true
+  sock = s0.accept  # Wait for a client to connect
+  Thread.new do
+    server sock
+  end
+end
+
 s0.close  # Close the server socket when done
